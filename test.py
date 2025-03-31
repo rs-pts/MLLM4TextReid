2c7e74c47399dbf952536157cb971600b7bf35c9
Avinash <82562956+Avinash-py@users.noreply.github.com>
2025-03-31 12:57:14 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -16,9 +16,12 @@ import argparse
 from utils.iotools import load_train_configs
 
 
+from utils.simple_tokenizer import SimpleTokenizer
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="IRRA Test")
-    parser.add_argument("--config_file", default='logs/CUHK-PEDES/iira/configs.yaml')
+    parser.add_argument("--config_file", default='/home/dslab/Documents/s24015/MLLM4Text-ReID/logs/RSTPReid/20250324_164152_baseline/configs.yaml')
     args = parser.parse_args()
     args = load_train_configs(args.config_file)
 
@@ -28,8 +31,12 @@ if __name__ == '__main__':
     device = "cuda"
 
     test_img_loader, test_txt_loader, num_classes = build_dataloader(args)
+    args.loss_names = 'sdm'
     model = build_model(args, num_classes=num_classes)
     checkpointer = Checkpointer(model)
-    checkpointer.load(f=op.join(args.output_dir, 'best.pth'))
+    checkpointer.load(f=op.join(args.output_dir, 'best2.pth'))
     model.to(device)
-    do_inference(model, test_img_loader, test_txt_loader)
+    tokenizer = SimpleTokenizer()
+    do_inference(model, test_img_loader, test_txt_loader, tokenizer)
+    
+    
